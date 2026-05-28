a708366a51529279d0b861137fc048fc700991f8
Artur Duarte Monteiro <monteiroarturduarte@gmail.com>
2026-05-28 09:30:17 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifgoiano/academico/sd_academico_aluno_service/SdAcademicoAlunoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.sd_academico_aluno_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdAcademicoAlunoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdAcademicoAlunoServiceApplication.class, args);
+	}
+
+}
